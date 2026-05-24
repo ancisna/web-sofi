ee4b6c4f104d1e90ce9f5482e94a1f471d2c8203
ancisna <32202065+ancisna@users.noreply.github.com>
2026-05-24 11:28:43 +0200

    2405 - cambio y testeo de estilos primeng

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'public-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+
+  imports: [RouterOutlet, RouterLink, ButtonModule],
+
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css',
 })
-export class PublicLayoutComponent {}
+export class PublicLayoutComponent {
+  mobileMenuOpen = signal(false);
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update((value) => !value);
+  }
+}
